46bbba6856a73797abf50cb66ccebd549e0aee28
bottkars <karsten.bott@bottnet.de>
2019-01-17 17:10:38 +0100

    loadbalancer

--- scripts/create_lb.sh
+++ scripts/create_lb.sh
@@ -9,32 +9,32 @@ az login --service-principal \
 
 az network public-ip create \
  --resource-group ${ENV_NAME} \
- --name ${CLUSTER}_ip \
+ --name ${CLUSTER}-public-ip \
  --sku standard \
  --allocation-method static
 
 az network lb create \
 --resource-group ${ENV_NAME} \
---name ${CLUSTER}_lb \
+--name ${CLUSTER}-lb \
 --sku standard
---public-ip-address ${CLUSTER}_ip \
---frontend-ip-name ${CLUSTER}_fe \
---backend-pool-name ${CLUSTER}_be      
+--public-ip-address ${CLUSTER}-public-ip \
+--frontend-ip-name ${CLUSTER}-fe \
+--backend-pool-name ${CLUSTER}-be      
 
 az network lb probe create \
---resource-group myResourceGroupSLB \
---lb-name myLoadBalancer \
---name myHealthProbe \
+--resource-group ${ENV_NAME} \
+--lb-name ${CLUSTER}-lb \
+--name ${CLUSTER}-probe-8443 \
 --protocol tcp \
 --port 8443
 
 az network lb rule create \
 --resource-group ${ENV_NAME} \
---lb-name ${CLUSTER}_lb \
+--lb-name ${CLUSTER}-lb \
 --name rule_8443 \
 --protocol tcp \
 --frontend-port any \
 --backend-port 8443 \
 --frontend-ip-name ${CLUSTER}_fe \
---backend-pool-name ${CLUSTER}_fbe \
---probe-name myHealthProbe  
+--backend-pool-name ${CLUSTER}_be \
+--probe-name ${CLUSTER}-probe-8443  
