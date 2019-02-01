527436b721f7fdfdc7dc31aef29955ae751f2dcf
bottkars <karsten.bott@bottnet.de>
2019-02-01 19:44:45 +0100

    updated docker from root install

--- scripts/deploy_docker.sh
+++ scripts/deploy_docker.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-source .env.sh
-MYSELF=$(basename $0)
-mkdir -p ${HOME_DIR}/logs
-exec &> >(tee -a "${HOME_DIR}/logs/${MYSELF}.$(date '+%Y-%m-%d-%H').log")
-exec 2>&1
 sudo groupadd docker
 sudo usermod -aG docker $ADMIN_USERNAME
 
