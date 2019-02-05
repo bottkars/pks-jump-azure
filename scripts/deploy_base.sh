910c2f66375059dce5bf150dc62406e1226b1214
bottkars <karsten.bott@bottnet.de>
2019-02-05 12:13:53 +0100

    grab master tfvars

--- scripts/deploy_base.sh
+++ scripts/deploy_base.sh
@@ -213,8 +213,8 @@ pcf_virtual_network_address_space = ["${NET_16_BIT_MASK}.0.0/16"]
 EOF
 # patch terraform for managed identity if tf is 0.29
 
-wget -q https://raw.githubusercontent.com/bottkars/pks-jump-azure/testing/patches/main.tf -O ./main.tf
-wget -q https://raw.githubusercontent.com/bottkars/pks-jump-azure/testing/patches/variables.tf -O ./variables.tf
+wget -q https://raw.githubusercontent.com/bottkars/pks-jump-azure/master/patches/main.tf -O ./main.tf
+wget -q https://raw.githubusercontent.com/bottkars/pks-jump-azure/master/patches/variables.tf -O ./variables.tf
 # end patch 
 
 
