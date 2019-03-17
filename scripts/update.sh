1db685ff4c73c10f04aecf0cd11ca244e2c3fb84
bottkars <karsten.bott@bottnet.de>
2019-03-17 09:57:21 +0100

    updates

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,7 +3,7 @@ source .env.sh
 MYSELF=$(basename $0)
 echo "this is the updater"
 mkdir -p ${LOG_DIR}
-UPDATE_DIR=${HOME}/conductor/updates/
+UPDATE_DIR=${HOME}/conductor/updates
 mkdir -p ${UPDATE_DIR}
 BASE_URI="https://raw.githubusercontent.com/bottkars/pks-jump-azure/master/"
 wget -P ${UPDATE_DIR} ${BASE_URI}/templates/updatefiles.txt
