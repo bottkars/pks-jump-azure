ae0f9b1fbbf485dbc102f0f48f7514cb4d7dab0f
bottkars <karsten.bott@bottnet.de>
2019-03-17 10:09:58 +0100

    updates

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,8 +5,8 @@ echo "this is the updater"
 mkdir -p ${LOG_DIR}
 UPDATE_DIR=${HOME}/conductor/updates
 mkdir -p ${UPDATE_DIR}
-BASE_URI="https://raw.githubusercontent.com/bottkars/pks-jump-azure/master"
-TEMPLATE_LIST=${BASE_URI}/templates/templates.txt
+BASE_URI="https://raw.githubusercontent.com/bottkars/pks-jump-azure/master/"
+TEMPLATE_LIST=${BASE_URI}templates/templates.txt
 wget -P ${UPDATE_DIR} ${TEMPLATE_LIST}
 cat ${UPADTE_DIR}/templates.txt | parallel --gnu "wget -P ${HOME}/conductor/templates {}"
 rm -rf ${UPADTE_DIR}/templates.txt
