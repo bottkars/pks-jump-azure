53250258e15b99dedc6d921046622d33d331b1aa
bottkars <karsten.bott@bottnet.de>
2019-07-17 12:07:45 +0200

    0.40 testing

--- scripts/om_init.sh
+++ scripts/om_init.sh
@@ -146,7 +146,7 @@ chown ${ADMIN_USERNAME}.${ADMIN_USERNAME} terraform.tfvars
 
 cd ./pivotal-cf-terraforming-azure-*/terraforming-pks
 
-PATCH_SERVER="https://raw.githubusercontent.com/bottkars/pks-jump-azure/master/patches/"
+PATCH_SERVER="https://raw.githubusercontent.com/bottkars/pks-jump-azure/testing/patches/0.40/"
 wget -q ${PATCH_SERVER}main.tf -O ./main.tf
 wget -q ${PATCH_SERVER}variables.tf -O ./variables.tf
 wget -q ${PATCH_SERVER}modules/pks/networking.tf -O ../modules/pks/networking.tf
